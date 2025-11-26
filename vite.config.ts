18fa4646606d9e6e0c85aaaf825e470ae1c5aae3
skanderbjaoui <bjaouiskander15@gmail.com>
2025-11-26 14:37:59 +0100

    render fix 4

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [angular()],
   server: {
     host: '0.0.0.0',
-    port: Number(process.env.PORT) || 10000,
+    port: Number(process.env.PORT) || 4200,
     allowedHosts: ['silianos.onrender.com'],
   },
 });
